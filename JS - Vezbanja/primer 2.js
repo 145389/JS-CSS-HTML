840fd3d1d3ca474ebe2a147d25a523c0a2ac3e35
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-21 13:30:39 +0200

    Petlja sa nizom

---
+++ JS - Vezbanja/primer 2.js
@@ -0,0 +1,13 @@
+let novcanice = [10,5,20,50,5,10,100,20,50,5,10,20];
+
+
+//length znaci da ce uzeti ovaj niz i prebrojati//
+let broj_novcanica = novcanice.length;
+
+console.log(novcanice.length);
+
+let izbrojane_novcanice = 0;
+
+while(izbrojane_novcanice< 11){
+  izbrojane_novcanice++;
+}
