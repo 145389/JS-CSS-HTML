77b9d05d4e3c689ce09ccafe7a62b7c442fdb566
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-21 14:49:21 +0200

    Verzija 003

--- JS - Vezbanja/primer 2.js
+++ JS - Vezbanja/primer 2.js
@@ -4,22 +4,11 @@ let broj_novcanica = novcanice.length
 let brojac = 0;
 
 while(brojac<broj_novcanica) {
-  console.log(novcanice[brojac]);
+
+  console.log('-------');
+  console.log('Uslov prosao');
+  console.log('nocanice na poziciji [${brojac}]: ${novcanice[brojac]}');
+
   brojac++
-
 }
 
-
-
-/*
-//length znaci da ce uzeti ovaj niz i prebrojati//
-let broj_novcanica = novcanice.length;
-
-console.log(broj_novcanica);
-
-let izbrojane_novcanice = 0;
-
-while(izbrojane_novcanice< broj_novcanica){
-  izbrojane_novcanice++;
-}
-console.log(' broj izbrojanih novcanica: ' + izbrojane_novcanice);*/
