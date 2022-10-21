658a413350d5b3e135317f98cef1b3851c4636ba
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-21 15:27:44 +0200

    Verzija 005 - for petlja

--- JS - Vezbanja/primer 2.js
+++ JS - Vezbanja/primer 2.js
@@ -1,17 +1,8 @@
 let novcanice = [10,5,20,50,5,10,100,20,50,];
 let broj_novcanica = novcanice.length
 let brojac = 0;
-while(brojac<broj_novcanica) {
-  console.log('-------');
-  console.log('Uslov prosao');
-  console.log('novcanica na poziciji [${brojac}]: $(novcanica[brojac]}');
-
-  brojac++
+// Postavljamo brojac na 0 ; postavljamo uslov ; inkrementacija brojaca;//
+// Unutar petlje stavljamo onaj deo koda koji zelimo da se ponavlja//
+for(brojac = 0; brojac < broj_novcanica; brojac++) {
+  console.log(novcanice[brojac]);
 }
-console.log('## KRAJ PETLJE ##')
- if(brojac < broj_novcanica) {
-  console.log('brojac je manji'); 
- } else {
-  console.log('brojac nije manji');
- }
-console.log('brojac:' + brojac);
