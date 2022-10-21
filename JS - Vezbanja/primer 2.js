4e51808d36e268897ace5d8291c81b3cf96da7e4
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-21 15:18:51 +0200

    Verzija 004 - while petlja

--- JS - Vezbanja/primer 2.js
+++ JS - Vezbanja/primer 2.js
@@ -1,14 +1,17 @@
 let novcanice = [10,5,20,50,5,10,100,20,50,];
-
 let broj_novcanica = novcanice.length
 let brojac = 0;
-
 while(brojac<broj_novcanica) {
-
   console.log('-------');
   console.log('Uslov prosao');
-  console.log('nocanice na poziciji [${brojac}]: ${novcanice[brojac]}');
+  console.log('novcanica na poziciji [${brojac}]: $(novcanica[brojac]}');
 
   brojac++
 }
-
+console.log('## KRAJ PETLJE ##')
+ if(brojac < broj_novcanica) {
+  console.log('brojac je manji'); 
+ } else {
+  console.log('brojac nije manji');
+ }
+console.log('brojac:' + brojac);
