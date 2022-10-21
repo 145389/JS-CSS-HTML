cf3751a080c02859532006546a2df1ebe1cffa9a
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-21 10:45:39 +0200

    Brojac novca

---
+++ JS - Vezbanja/Petlja.js
@@ -0,0 +1,24 @@
+let novcanice = 35; 
+
+
+// 10 eura  vrednost jedne novcanice 
+let vrednost_novcanice = 10; 
+
+//brojac
+let brojac = 0;
+
+// izbrojane novcanice
+let izbrojane_novcanice =0;
+
+
+/* Broji novcanice sve dok je brojac manji od 
+broja novcanica*/
+
+while (brojac < novcanice){
+  izbrojane_novcanice++
+
+  brojac++
+}
+
+console.log(izbrojane_novcanice);
+
