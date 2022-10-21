178a8e5401b46f9911c2951ae5b469c39ca1a1ba
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-21 13:37:22 +0200

    Verzija 001

--- JS - Vezbanja/primer 2.js
+++ JS - Vezbanja/primer 2.js
@@ -1,13 +1,14 @@
-let novcanice = [10,5,20,50,5,10,100,20,50,5,10,20];
+let novcanice = [10,5,20,50,5,10,100,20,50,];
 
 
 //length znaci da ce uzeti ovaj niz i prebrojati//
 let broj_novcanica = novcanice.length;
 
-console.log(novcanice.length);
+console.log(broj_novcanica);
 
 let izbrojane_novcanice = 0;
 
-while(izbrojane_novcanice< 11){
+while(izbrojane_novcanice< broj_novcanica){
   izbrojane_novcanice++;
 }
+console.log(' broj izbrojanih novcanica: ' + izbrojane_novcanice);
