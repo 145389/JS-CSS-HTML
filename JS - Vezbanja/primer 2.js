d070309955d93cd6afa6beed51ba1209b7d1c5bd
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-26 09:18:35 +0200

    Verzija 006

--- JS - Vezbanja/primer 2.js
+++ JS - Vezbanja/primer 2.js
@@ -1,8 +1,14 @@
 let novcanice = [10,5,20,50,5,10,100,20,50,];
 let broj_novcanica = novcanice.length
-let brojac = 0;
+let ukupno_novca = 0;
 // Postavljamo brojac na 0 ; postavljamo uslov ; inkrementacija brojaca;//
 // Unutar petlje stavljamo onaj deo koda koji zelimo da se ponavlja//
 for(brojac = 0; brojac < broj_novcanica; brojac++) {
   console.log(novcanice[brojac]);
+
+  // REZULTAT JE 50 ZATO STO SVAKI PUT UKUPNO_NOVCA DOBIJA NOVU VREDNOST//
+  ukupno_novca = novcanice[brojac];
+
 }
+console.log(' ## KRAJ PETLJE ##')
+console.log(ukupno_novca)
