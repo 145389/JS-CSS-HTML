3f392b2722986e686d58a1b311f1321de47ba004
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-10-26 23:58:43 +0200

    Verzija 009 - vise primera

--- JS - Vezbanja/primer 2.js
+++ JS - Vezbanja/primer 2.js
@@ -1,12 +1,84 @@
-let novcanice = [200,10,5,20,50,5,10,100,20,50,];
-let broj_novcanica = novcanice.length
-let ukupno_novca = 0;
-  
-//Jednostavniji nacin Verzije 006 //
 
-for(let brojac in novcanice) {
-console.log(brojac);
-console.log(novcanice[brojac]);
-console.log( '------------')
-  
+/*let novcanice = [200,10,5,20,50,5,10,100,20,50,];
+
+for(let novcanica of  novcanice) {
+  console.log(novcanica);
+} */
+ 
+/* let automobili = ['Audi', 'BMW', 'Porsche', 'Mercedes'];
+
+for (let automobil of automobili ) {
+  console.log(automobil);
+} */ 
+
+/* let automobili = ['Audi', 'BMW', 'Porsche', 'Mercedes'];
+
+automobili.forEach(function (automobil) {
+  console.log(automobil);
+
+}); */
+/*
+let automobili = { 
+  prvi_auto: 'Audi', 
+  drugi_auto: 'BMW', 
+  treci_auto: 'Porsche',
+  cetvrti_auto: 'Mercedes'
+};
+console.log(automobili);  */
+
+  //Ovo je primer kada imamo jednu informaciju //
+  /*
+let automobili = { 
+  audi: '50.000',
+  bmw: '60.000',
+  porsche: '70.000',
+  mercedes: '80.000'
+};
+
+console.log(automobili);*/
+
+ //Primer sa vise informacija [za svaki pojedinacno mozemo staviti niz] // 
+
+//Definisani objekti//
+/*let automobili = { 
+  audi: ['70.000', '2017', 'Q7' ],
+  bmw: ['50.000', '2018', '330D'],
+  porsche: ['60.000', '2016', 'Boxter'],
+  mercedes: ['100.000', '2019', '5400']
+};
+
+console.log(automobili);*/
+
+
+// Koristeci se for petljom dobijamo listu niza  // 
+/*
+let automobili = { 
+  audi: ['70.000', '2017', 'Q7' ],
+  bmw: ['50.000', '2018', '330D'],
+  porsche: ['60.000', '2016', 'Boxter'],
+  mercedes: ['100.000', '2019', '5400']
+};
+
+for(automobil in automobili) {
+  console.log(automobil)
+  console.log(automobili[automobil]);
+  console.log( '-----------------')
+}*/
+
+let automobili = { 
+  audi: ['70.000', '2017', 'Q7' ],
+  bmw: ['50.000', '2018', '330D'],
+  porsche: ['60.000', '2016', 'Boxter'],
+  mercedes: ['100.000', '2019', '5400']
+};
+
+for(automobil in automobili) {
+ 
+  console.log(automobili[automobil]);
+  console.log( '-----------------')
 }
+
+automobili.audi.forEach(element => {
+  console.log(element);
+});
+
