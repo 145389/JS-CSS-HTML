40991b1de86370bfbd2d14e7e6a34e75e09ebf0f
Zeljko <cvetkovic.zeljko991@gmail.com>
2022-11-05 08:49:05 +0100

    Verzija 019 - pozivanje

--- JS - Vezbanja/primer .js
+++ JS - Vezbanja/primer .js
@@ -1,5 +1,42 @@
+/*
 let brend = document.getElementsByTagName('span');
 
-console.log(brend[0].innerText);
-console.log(brend[1].innerText);
-console.log(brend[2].innerText);
+for (let auto of brend ) {
+console.log(auto.innerText);
+};
+
+console.log('----------------------')
+let modeli = document.getElementsByClassName('automobil-model');
+
+
+for (let model of modeli ) {
+  console.log(model.innerText);
+  };
+
+  console.log('----------------------')
+  let najbolji = document.getElementById('recenica');
+  console.log(najbolji.innerText);
+
+  console.log('----------------------')
+*/
+
+
+let brend = document.querySelectorAll('span')
+
+for (let auto of brend) {
+console.log(auto.innerText);
+
+};
+console.log('----------------------')
+let modeli = document.querySelectorAll('.automobil-model');
+
+
+for (let model of  modeli) {
+
+console.log(model.innerText);
+
+};
+console.log('----------------------')
+let recenica = document.querySelector('#recenica');
+
+console.log(recenica.innerText);
